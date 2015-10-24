b30cd8fa6f98b2e4a901f6d36b942e75a6ad1287
bangwww <bang1234@mail.ru>
2015-10-25 00:26:29 +0300

    Добавлен класс Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,13 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+            Console.ReadKey();
         }
+        
     }
 }
